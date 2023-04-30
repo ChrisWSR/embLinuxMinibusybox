77d0893b21f861427e04817f63d7d9d4ed30480d
Chris <william.sanchez@alumnos.udg.mx>
2023-04-29 22:05:00 -0600

    Actualizar el contenido de la rama my_uname

--- my_uname/my_uname.c
+++ my_uname/my_uname.c
@@ -17,3 +17,20 @@ int my_uname(struct utsname *buf) {
 
     return ret;
 }
+
+int main(int argc, char *argv[]) {
+    struct utsname buf;
+
+    if (my_uname(&buf) == 0) {
+        printf("System name: %s\n", buf.sysname);
+        printf("Node name: %s\n", buf.nodename);
+        printf("Release: %s\n", buf.release);
+        printf("Version: %s\n", buf.version);
+        printf("Machine: %s\n", buf.machine);
+    } else {
+        fprintf(stderr, "Error getting system information.\n");
+        return 1;
+    }
+
+    return 0;
+}
