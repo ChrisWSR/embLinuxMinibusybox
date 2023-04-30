feabdd39b30172197d5c3dfd2bda15d8ac216a43
Chris <william.sanchez@alumnos.udg.mx>
2023-04-30 14:26:15 -0600

    I modify the function my_sleep so that it does not have main function

--- my_uname/my_uname.h
+++ my_uname/my_uname.h
@@ -1,8 +1,10 @@
 #ifndef MY_UNAME_H
 #define MY_UNAME_H
-
+#include <stdio.h>
+#include <string.h>
+#include <errno.h> 
 #include <sys/utsname.h>
 
-int my_uname(struct utsname *buf);
+int my_uname();
 
 #endif // MY_UNAME_H
